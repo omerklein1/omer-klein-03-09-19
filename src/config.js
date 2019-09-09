6c26bbdee4ad701710844afa24fb0b276b47ba65
omerklein1 <oklein01@gmail.com>
2019-09-09 11:53:58 +0300

    make it better

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const apikey = /* 'fGGvAwbDbJnHAb7LWYV3oyw8cB2Az8hR' */ 'yxA1s4JrJ2XJWo2YOcdUfqOY7Wi7Mk8y'
+export const apikey = /* 'fGGvAwbDbJnHAb7LWYV3oyw8cB2Az8hR' */ /* 'yxA1s4JrJ2XJWo2YOcdUfqOY7Wi7Mk8y' */ 'A3oFXg6338nmKAcmr03x4TAv8ZMrfI79'
 export const webSite = 'http://dataservice.accuweather.com'
 
